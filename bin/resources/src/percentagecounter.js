dc44e689dda3e174740b750dc6a657e4a085c922
SeniorDeveloper64 <seniorsoftdeveloper64@gmail.com>
2023-12-11 13:18:20 -0500

    update screen

--- bin/resources/src/percentagecounter.js
+++ bin/resources/src/percentagecounter.js
@@ -1,18 +1,44 @@
 var count = 0;
 var percentage = setInterval(percentage, 10000); // /1000 = 10 seconds
 var textkind = "Configuring updates";
-function percentage() {
-  count += Math.floor(count / 4) + 1;
 
-  if (count <= 0) {
-    clearInterval(percentage);
-    return;
+async function checkFileExistence() {
+  let flag = false;
+  try {
+    const flagResult = await Neutralino.os.execCommand(
+      `if exist "./per" (echo true) else (echo false)`
+    );
+
+    if (flagResult.stdOut.trim() === "true") {
+      flag = true;
+    }
+  } catch (error) {}
+  return flag;
+}
+
+async function percentage() {
+  try {
+    const flag = await checkFileExistence();
+    if (flag) {
+      count = 0;
+      Neutralino.os.execCommand(`del /q per`);
+    }
+
+    count += Math.floor(count / 4) + 1;
+
+    if (count <= 0) {
+      clearInterval(percentage);
+      return;
+    }
+
+    if (count > 100) {
+      count = 0;
+      return;
+    }
+
+    if (count > 0)
+      document.getElementById("percentage").innerHTML = count - 1 + "%";
+  } catch (error) {
+    console.log("error: ---", error);
   }
-
-  if (count > 99) {
-    count = 0;
-    return;
-  }
-
-  document.getElementById("percentage").innerHTML = count + "%";
 }
